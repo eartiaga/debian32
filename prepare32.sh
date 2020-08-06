71197d58ad051b330a3ff328de74d0128ff101a7
Ernest Artiaga <ernest.artiaga@eartiam.net>
2020-08-06 16:11:19 +0200

    Workaround for libgcc1 vs. libgcc-s1 conflict
    Temporary patch to force the exclusion of the
    obsolete libgcc1 package, which conflicts with
    the newer libgcc-s1 package in debian sid
    
    Signed-off-by: Ernest Artiaga <ernest.artiaga@eartiam.net>

--- prepare32.sh
+++ prepare32.sh
@@ -6,11 +6,13 @@ set -x
 export OUTFILE="$1"
 [ -n "$OUTFILE" ] || export OUTFILE="debian32.tgz"
 export OUTFILE="$(readlink -f "$OUTFILE")"
+export TSTAMP=$(date "+%s")
 
 export ARCH=i386
 export SUITE=sid
-export MIRROR="http://debian.grn.cat/debian"
-export IMAGEDIR="/tmp/chroot.$(date "+%z").$$"
+export MIRROR="http://deb.debian.org/debian"
+#export MIRROR="http://debian.grn.cat/debian"
+export IMAGEDIR="/tmp/chroot.${TSTAMP}.$$"
 cleanup() {
     rm -rf "$IMAGEDIR"
 }
@@ -24,16 +26,24 @@ if [ -e "$OUTFILE" ]; then
     exit 1
 fi
 
-DEBOOTSTRAP=$(command -v "debootstrap")
-if [ -z "$DEBOOTSTRAP" ]; then
+#DEBOOTSTRAP=$(command -v "debootstrap")
+DEBOOTSTRAP=$(readlink -f "./scripts/debootstrap")
+if [ -z "$DEBOOTSTRAP" -o ! -x "$DEBOOTSTRAP" ]; then
     echo "missing debootstrap command"
+    exit 1
+fi
+
+TAR=$(command -v "tar")
+if [ -z "$TAR" -o ! -x "$TAR" ]; then
+    echo "missing tar command"
     exit 1
 fi
 
 mkdir -p "$IMAGEDIR/image"
 $DEBOOTSTRAP --arch $ARCH \
-    --include=apt,aptitude \
+    --include=apt,aptitude,libgcc-s1 \
+    --exclude=libgcc1 \
     $SUITE "$IMAGEDIR/image" $MIRROR 
 
-tar czf "$OUTFILE" -C "$IMAGEDIR/image" .
+$TAR czf "$OUTFILE" -C "$IMAGEDIR/image" .
 
